a91f249be0e1f25ec2dd33ce22fb34440e4a10b6
Frédéric Bertolus <fred.bertolus@gmail.com>
2020-04-16 18:55:08 +0200

    Begin to bind game manager

--- gui/gui_root.gd
+++ gui/gui_root.gd
@@ -3,16 +3,18 @@ extends Control
 export(NodePath) var aurora_world_path
 export(NodePath) var aurora_world_renderer_path
 export(NodePath) var aurora_world_editor_path
+export(NodePath) var aurora_game_manager_path
 
 var world_render_stats_label : Label
 var world_update_stats_label : Label
 var aurora_world : AuroraWorld
 var aurora_world_renderer : AuroraWorldRenderer
 var aurora_world_editor : AuroraWorldEditor
+var game_manager : AuroraGameManager
 
 var smooth_last_render_duration_us : float
 # TODO max
-
+var has_game : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,6 +23,9 @@ func _ready():
 	self.aurora_world = get_node(self.aurora_world_path)
 	self.aurora_world_renderer = get_node(self.aurora_world_renderer_path)
 	self.aurora_world_editor = get_node(self.aurora_world_editor_path)
+	self.game_manager = get_node(self.aurora_game_manager_path)
+	has_game = false
+	self.update_state()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -31,3 +36,30 @@ func _process(delta):
 
 	world_render_stats_label.text = "World render: %5.2f ms" % (self.smooth_last_render_duration_us * 0.001)
 	world_update_stats_label.text = "World update: %5.2f ms" % (update_duration_us * 0.001)
+
+	var new_has_game = game_manager.has_game()
+	if new_has_game != self.has_game:
+		self.has_game = new_has_game
+		self.update_state()
+
+func update_state():
+	var save_game_button = get_node("MainMenu/VBoxContainer/SaveButton")
+	var load_game_button = get_node("MainMenu/VBoxContainer/LoadButton")
+	save_game_button.disabled = !self.has_game
+	load_game_button.disabled = !self.has_game
+
+func _on_SaveButton_pressed():
+	self.game_manager.get_game().save()
+
+func _on_LoadButton_pressed():
+	self.game_manager.load(0)
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+
+
+
+func _on_NewButton_pressed():
+	self.game_manager.create_game()
