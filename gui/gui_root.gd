45ada91a745ca30bb1aea31b0fb07676a96c1616
Frédéric Bertolus <fred.bertolus@gmail.com>
2020-04-26 16:22:43 +0200

    Begin to adapt to new game manager

--- gui/gui_root.gd
+++ gui/gui_root.gd
@@ -1,15 +1,11 @@
 extends Control
 
-export(NodePath) var aurora_world_path
 export(NodePath) var aurora_world_renderer_path
-export(NodePath) var aurora_world_editor_path
 export(NodePath) var aurora_game_manager_path
 
 var world_render_stats_label : Label
 var world_update_stats_label : Label
-var aurora_world : AuroraWorld
-var aurora_world_renderer : AuroraWorldRenderer
-var aurora_world_editor : AuroraWorldEditor
+var world_renderer : AuroraWorldRenderer
 var game_manager : AuroraGameManager
 
 var smooth_last_render_duration_us : float
@@ -20,22 +16,24 @@ var has_game : bool
 func _ready():
 	self.world_render_stats_label = get_node("DebugPanel/WorldRenderStatsLabel")
 	self.world_update_stats_label = get_node("DebugPanel/WorldUpdateStatsLabel")
-	self.aurora_world = get_node(self.aurora_world_path)
-	self.aurora_world_renderer = get_node(self.aurora_world_renderer_path)
-	self.aurora_world_editor = get_node(self.aurora_world_editor_path)
+	self.world_renderer = get_node(self.aurora_world_renderer_path)
 	self.game_manager = get_node(self.aurora_game_manager_path)
 	has_game = false
 	self.update_state()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var render_duration_us = aurora_world_renderer.get_last_render_duration()
-	var update_duration_us = aurora_world.get_last_update_duration()
+	var render_duration_us = world_renderer.get_last_render_duration()
+	if(game_manager.has_game()):
+		var update_duration_us = game_manager.get_game().get_world().get_last_update_duration()
+		world_update_stats_label.text = "World update: %5.2f ms" % (update_duration_us * 0.001)
+	else:
+		world_update_stats_label.text = "World update: no world"
 
 	self.smooth_last_render_duration_us = lerp(self.smooth_last_render_duration_us, render_duration_us, 0.9)
 
 	world_render_stats_label.text = "World render: %5.2f ms" % (self.smooth_last_render_duration_us * 0.001)
-	world_update_stats_label.text = "World update: %5.2f ms" % (update_duration_us * 0.001)
+	
 
 	var new_has_game = game_manager.has_game()
 	if new_has_game != self.has_game:
@@ -47,6 +45,9 @@ func update_state():
 	var load_game_button = get_node("MainMenu/VBoxContainer/LoadButton")
 	save_game_button.disabled = !self.has_game
 	load_game_button.disabled = !self.has_game
+	
+	var editor_panel = get_node("EditorPanel")
+	editor_panel.set_visible(self.has_game)
 
 func _on_SaveButton_pressed():
 	self.game_manager.get_game().save()
--- world.gd
+++ world.gd
@@ -1,18 +1,15 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var free_camera : Camera2D
 var player_camera : Camera2D
 var use_free_camera = true
-var world = AuroraWorld
+var game_manager = AuroraGameManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	free_camera = get_node("FreeCamera")
 	player_camera = get_node("test_player/Camera")
-	world = get_node("AuroraWorld")
+	game_manager = get_node("AuroraGameManager")
 	update_current_camera()
 	pass # Replace with function body.
 
@@ -22,11 +19,14 @@ func _process(delta):
 		use_free_camera = !use_free_camera
 		update_current_camera()
 		
-	if Input.is_action_just_pressed("game_step"):
-		world.step()
-
-	if Input.is_action_just_pressed("game_toogle_pause"):
-		world.set_pause(not world.is_paused())
+		
+	if self.game_manager.has_game():
+		var game = self.game_manager.get_game()
+		if Input.is_action_just_pressed("game_step"):
+			game.step()
+	
+		if Input.is_action_just_pressed("game_toogle_pause"):
+			game.set_pause(not game.is_paused())
 
 func update_current_camera():
 	if use_free_camera:
